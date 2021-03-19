ddd210968bbdb4edfdea1f03f851801ff7e9514b
Shandira <sduxe23@gmail.com>
2021-03-18 23:33:57 -0700

    Hold button logic added

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ score1Elem.textContent = 0;
 diceElem.classList.add('hidden'); //Hide the mid dice
 let currScore = 0;
 let activePlayer = 0;
+let totalScores = [0, 0];
+
+//Switch player funcitonality
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player1.classList.toggle('player--active');
+  player0.classList.toggle('player--active');
+  currScore = 0;
+};
 
 /* ---- Roll Dice functionality ----- */
 btnRoll.addEventListener('click', function () {
@@ -37,12 +47,28 @@ btnRoll.addEventListener('click', function () {
     // curr0Elem.textContent = currScore;
   } else {
     //Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player1.classList.toggle('player--active');
-    player0.classList.toggle('player--active');
-    currScore = 0;
+    // document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // activePlayer = activePlayer === 0 ? 1 : 0;
+    // player1.classList.toggle('player--active');
+    // player0.classList.toggle('player--active');
+    // currScore = 0;
+    switchPlayer();
   }
 });
 
 // * ---------- Hold button functionality -------------- *
+btnHold.addEventListener('click', function () {
+  if (activePlayer === 0) {
+    totalScores[0] += currScore;
+    console.log(`totalScores of 0 is ${totalScores[0]}`);
+    score0Elem.textContent = totalScores[0];
+    //Switch to next player
+    switchPlayer();
+  } else {
+    totalScores[1] += currScore;
+    console.log(`totalScores of 1 is ${totalScores[1]}`);
+    score1Elem.textContent = totalScores[1];
+    //Switch to next player
+    switchPlayer();
+  }
+});
